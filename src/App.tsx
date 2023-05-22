4b49b86f4472275981fc9ec97ccae764081bf678
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-05-22 12:40:05 -0300

    memory game created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from "./App.styles";
+import logoImg from "./assets/devmemory_logo.png";
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Info>
+        <C.LogoLink>
+          <img src={logoImg} width="200" alt="" />
+        </C.LogoLink>
+
+        <C.InfoArea>...</C.InfoArea>
+        <button>Reiniciar</button>
+      </C.Info>
+      <C.GridArea></C.GridArea>
+    </C.Container>
   );
 }
-
-export default App;
